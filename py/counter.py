06e28ca8957340d90f3516011dfdabbeabe015a9
mehtank <mehtank>
2023-12-01 13:18:34 -0800

    Added callback functionality to counter

--- py/counter.py
+++ py/counter.py
@@ -1,11 +1,12 @@
 class Counter:
-    def __init__(self, pin, init=0, rising=True, falling=False, d=1):
+    def __init__(self, pin, init=0, rising=True, falling=False, d=1, callback=None):
         self._pin = pin
         self._last = pin.value()
         self._init = init
         self._rising = rising
         self._falling = falling
         self._d = d
+        self._callback = callback
 
         self.reset()
 
@@ -29,5 +30,21 @@ class Counter:
         trig = (self._rising and not self._last and val) or (self._falling and self._last and not val)
         if trig:
             self.value += self._d
+            if self._callback:
+                self._callback(self.value, val)
         self._last = val
 
+if __name__ == "__main__":
+    from machine import Pin
+    import time
+
+    def callback(value, pin):
+        print(f"Pin became {pin} to get {value}")
+
+    boot = Pin(0, Pin.IN) # Boot button
+    c = Counter(boot, callback=callback)
+
+    while True:
+        c.update()
+        print(c.value)
+        time.sleep(0.1)
