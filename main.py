ba2c9fd572d08e61edd81d21cd646d9714999f2d
mehtank <mehtank>
2023-12-08 13:09:57 -0800

    Trying serial code

--- main.py
+++ main.py
@@ -1,78 +1,75 @@
+import machine
 import json
-import uasyncio as a
+import time
 
-from py.wifi import connect
-from py.esphub import ESPHub
 from py.silversend import Silversend
 
-sids = set()
+uart = machine.UART(0, 115200)
+uart.init(115200, bits=8, parity=None, stop=1)
 
+led = machine.Pin(2, machine.Pin.OUT)
+led.on()
 
+def rx():
+    if uart.any():
+        data = uart.readline()
+        return json.load(data)
+    return None
 
-with open("config.json") as f:
-    config = json.load(f)
-wificonf = config["wifi"]
-esphubconf = config["esphub"]
+def send(event, data):
+    uart.write(json.dumps([event, data]))
 
-async def main():
-    wifi = await connect(**wificonf)
-    hub = ESPHub(**esphubconf, led_pin=config["led_pin"])
-    await hub.connect()
+def rowstarting(row):
+    send('rowstart', row)
 
-    def rowstarting(row):
-        # print(f"now on row {row}")
-        # Socket IO send event "rowstart" with data=row to every client in array sids
-        hub.rowstart(sids, row)
+def rowcomplete(row):
+    send('rowfinish', row)
 
-    def rowcomplete(row):
-        print(f"row {row} completed!")
-        # Socket IO send event "rowfinish" with data=row to every client in array sids
-        hub.rowfinish(sids, row)
+'''
+s = Silversend(-20, 20, rowstarting=rowstarting, rowcomplete=rowcomplete)
+pattern = [0, 1] * 20
+s.loadrow(pattern)
 
-    s = Silversend(-20, 20, rowstarting=rowstarting, rowcomplete=rowcomplete)
+print("we can do things and we're starting")
 
-    pattern = [0, 1] * 20
-    s.loadrow(pattern)
+# Socket IO on event... this is also how you make more events
+@hub.on("setcams")
+async def row(data, sid):
+    s.setcams(*data)
+    print(f"we are resetting the cam positions to {data}. Dope!")
 
-    print("we can do things and we're starting")
+# Socket IO on event... this is also how you make more events
+@hub.on("setrowindex")
+async def row(data, sid):
+    s.setrowindex(data)
+    print(f"we are resetting the row counter to {data}. Dope!")
 
-    # Socket IO on event... this is also how you make more events
-    @hub.on("setcams")
-    async def row(data, sid):
-        s.setcams(*data)
-        print(f"we are resetting the cam positions to {data}. Dope!")
+# Socket IO on event... this is also how you make more events
+@hub.on("loadrow")
+async def row(data, sid):
+    s.loadrow(data)
+    print(f"we got a row, which is {data}. Dope!")
 
-    # Socket IO on event... this is also how you make more events
-    @hub.on("setrowindex")
-    async def row(data, sid):
-        s.setrowindex(data)
-        print(f"we are resetting the row counter to {data}. Dope!")
+# Socket IO on event... this is also how you make more events
+@hub.on("subscribe")
+async def subscribe(data, sid):
+    sids.add(sid)
+    print(f"we added {sid}, they are now all {sids}!")
 
-    # Socket IO on event... this is also how you make more events
-    @hub.on("loadrow")
-    async def row(data, sid):
-        s.loadrow(data)
-        print(f"we got a row, which is {data}. Dope!")
-
-    # Socket IO on event... this is also how you make more events
-    @hub.on("subscribe")
-    async def subscribe(data, sid):
-        sids.add(sid)
-        print(f"we added {sid}, they are now all {sids}!")
-
-    # Socket IO on event... this is also how you make more events
-    @hub.on("unsubscribe")
-    async def subscribe(data, sid):
-        sids.remove(sid)
-        print(f"we removed {sid}, they are now all {sids}!")
-
-    while True:
-        try:
-            s.update()
-            await a.sleep(0)
-        except KeyboardInterrupt:
-            print("Received exit, exiting")
-            hub.close()
-            break
+# Socket IO on event... this is also how you make more events
+@hub.on("unsubscribe")
+async def subscribe(data, sid):
+    sids.remove(sid)
+    print(f"we removed {sid}, they are now all {sids}!")
 
 a.run(main())
+'''
+
+while True:
+    try:
+        send('got', [0, 1])
+        time.sleep(1)
+        led.value(1-led.value())
+    except KeyboardInterrupt:
+        break
+
